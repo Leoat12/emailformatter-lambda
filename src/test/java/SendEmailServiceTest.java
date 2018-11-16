d96222e3fb36ad85116e945e97ee7929332839d6
Leonardo <leoat12@gmail.com>
2018-11-15 23:04:14 -0200

    Diagram image.

--- src/test/java/SendEmailServiceTest.java
+++ src/test/java/SendEmailServiceTest.java
@@ -11,25 +11,25 @@ import static org.hamcrest.core.IsNot.not;
 
 public class SendEmailServiceTest {
 
-//    @Test
-//    public void testEmailSending() throws IOException {
-//        SendEmailService sendEmailService = new SendEmailService();
-//
-//        Properties props = new Properties();
-//        props.load(SendEmailServiceTest.class.getClassLoader().getResourceAsStream("function.properties"));
-//
-//        Map<String, Object> data = new HashMap<>();
-//        data.put("USERNAME", "Leonardo");
-//        data.put("email.source", props.getProperty("email.source"));
-//        data.put("email.destination", props.getProperty("email.destination"));
-//        data.put("email.subject", props.getProperty("email.subject"));
-//        data.put("s3.bucketName", props.getProperty("s3.bucketName"));
-//        data.put("s3.filePath", props.getProperty("s3.filePath"));
-//
-//        String messageId = sendEmailService.sendEmailFromLocal(data);
-//
-//        assertThat(messageId, not(equalTo(null)));
-//    }
+    @Test
+    public void testEmailSending() throws IOException {
+        SendEmailService sendEmailService = new SendEmailService();
+
+        Properties props = new Properties();
+        props.load(SendEmailServiceTest.class.getClassLoader().getResourceAsStream("function.properties"));
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("USERNAME", "Leonardo");
+        data.put("email.source", props.getProperty("email.source"));
+        data.put("email.destination", props.getProperty("email.destination"));
+        data.put("email.subject", props.getProperty("email.subject"));
+        data.put("s3.bucketName", props.getProperty("s3.bucketName"));
+        data.put("s3.filePath", props.getProperty("s3.filePath"));
+
+        String messageId = sendEmailService.sendEmailFromLocal(data);
+
+        assertThat(messageId, not(equalTo(null)));
+    }
 
 
 }
